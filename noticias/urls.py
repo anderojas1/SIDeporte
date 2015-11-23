82cd94000fb3a0adeb7a3feb63818848a5e09274
Anderson Enríquez Rojas <anderojas@localhost.localdomain>
2015-11-23 00:19:54 -0500

    url de creación hecho y vista para el registro

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
-from .views import DetallesNoticia, BuscarNoticias
+from .views import DetallesNoticia, BuscarNoticias, RegistrarNoticia
 
 urlpatterns =  patterns ('',
     url(r'^(?P<id_noticias>\d+)/$', DetallesNoticia.as_view(), name='detalles_noticia'),
     url(r'^buscar$', BuscarNoticias.as_view(), name='buscar_noticias'),
+    url(r'^registrar$', RegistrarNoticia.as_view(), name='registrar_noticia')
 )
--- noticias/views.py
+++ noticias/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from .models import Noticias
+from .forms import FormRegistroNoticias
 # Create your views here.
 
 
@@ -11,6 +12,7 @@ class DetallesNoticia(TemplateView):
 
 	def get_context_data(self, **kwargs):
 		context = super(DetallesNoticia, self).get_context_data(**kwargs)
+		print(kwargs['id_noticias'])
 
 		self.noticia = Noticias.objects.get(pk=self.kwargs['id_noticias'])
 		if 'noticia' not in context:
@@ -28,6 +30,33 @@ class BuscarNoticias(TemplateView):
 
 		self.noticias = Noticias.objects.all()
 		context['noticias'] = self.noticias
-		print(context['noticias'][0].imagen.url)
 
 		return context
+
+
+class RegistrarNoticia(TemplateView):
+	template_name = 'noticias/registrar_noticia.html'
+	form_registrar_noticia = FormRegistroNoticias()
+
+	def get_context_data(self, **kwargs):
+		context = super(RegistrarNoticia, self).get_context_data(**kwargs)
+
+		context['form'] = self.form_registrar_noticia
+		return context
+
+	def post(self, request, *args, **kwargs):
+		context = super(RegistrarNoticia, self).get_context_data(**kwargs)
+		self.form_registrar_noticia = FormRegistroNoticias(request.POST, request.FILES)
+		print(self.form_registrar_noticia)
+
+		if self.form_registrar_noticia.is_valid():
+			print('es válido')
+			self.form_registrar_noticia.save()
+			context['exito'] = 'La noticia ha sido registrada exitosamente'
+
+			return render(request, self.template_name, context)
+
+		else:
+			context['form'] = self.form_registrar_noticia
+			return render(request, self.template_name, context)
+
