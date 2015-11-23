d0a861822f3551315a1cb92c9507876e7e4b7897
Anderson Enríquez Rojas <anderojas@localhost.localdomain>
2015-11-23 00:21:05 -0500

    adecuación de la clase Noticias para registrar el código automáticamente

--- noticias/models.py
+++ noticias/models.py
@@ -1,16 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
 
 class Noticias (models.Model):
 	codigo = models.CharField(max_length=20, primary_key=True)
 	titulo = models.CharField(max_length=100)
-	fecha = models.DateField()
+	fecha = models.DateField(editable=False)
 	imagen = models.ImageField(upload_to='imagenes')
 	contenido = models.CharField(max_length=1000)
 
 	def __str__(self):
 		return self.titulo
+
+
+	def save(self, *args, **kwargs):
+		fecha = timezone.now().date()
+		try:
+			consecutivo = str(len(Noticias.objects.filter(fecha=fecha)))
+			codigo = str(fecha) + consecutivo
+			codigo = codigo.replace('-', '')
+		except ObjectDoesNotExist:
+			codigo = str(fecha) + "0"
+			codigo = codigo.replace('-', '')
+		print(codigo)
+		self.fecha = fecha
+		self.codigo = codigo
+
+		print('aqui estoy')
+
+		return super(Noticias, self).save(*args, **kwargs)
+
 
 	@models.permalink
 	def get_absolute_url(self):
@@ -18,3 +39,4 @@ class Noticias (models.Model):
 
 	class Meta:
 		verbose_name_plural = "Noticias"
+		ordering = ['fecha']
