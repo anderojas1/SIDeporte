30311e9afc9488b8bd0193d1e252886658f8f0c3
Anderson Enríquez Rojas <anderojas@localhost.localdomain>
2015-11-29 02:01:14 -0500

    inclusión del módulo de reportes

--- SIDeporte/settings.py
+++ SIDeporte/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'noticias',
     'Coldeportes',
+    'reportes',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -91,9 +92,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
