820cddc72f80c5ed54fb10a902e3fd215b54ba48
Anderson Enríquez Rojas <anderojas@new-host-2.home>
2016-02-14 03:43:31 -0500

    reporte de ranking nacional por deporte linkeado desde menú de reporte en tablas; se agregar botón de descarga de reporte en PDF; se define la clase y vista para descargar el PDF

--- reportes/views.py
+++ reportes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, render_to_response
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+from io import BytesIO
 from django.views.generic.edit import DeleteView, UpdateView
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, inch
@@ -12,7 +14,7 @@ from Coldeportes.models import *
 from Coldeportes.grupos import InformacionUsuario
 
 
-class MyPrint:
+class PDFReport:
 
     def __init__(self, buffer, pagesize):
         self.buffer = buffer
@@ -22,29 +24,32 @@ class MyPrint:
             self.pagesize = letter        
         self.width, self.height = self.pagesize
 
-    def print_users(self):
+    def print_elements(self, lista, encabezado):
 
-    	buffer = self.buffer
-    	doc = SimpleDocTemplate(buffer,
-			rightMargin=72,
-			leftMargin=72,
-			topMargin=72,
-			bottomMargin=72,
-			pagesize=self.pagesize)
+        buffer = self.buffer
+        doc = SimpleDocTemplate(buffer,
+            rightMargin=72,
+            leftMargin=72,
+            topMargin=72,
+            bottomMargin=72,
+            pagesize=self.pagesize)
 
-    	elements = []
-    	data = []
+        elements = []
+        data = []
 
-    	styles = getSampleStyleSheet()
-    	styles.add(ParagraphStyle(name='centered', alignment=TA_CENTER))
+        styles = getSampleStyleSheet()
+        styles.add(ParagraphStyle(name='centered', alignment=TA_CENTER))
 
-    	users = User.objects.all()
-    	elements.append(Paragraph('My User Names', styles['Heading1']))
-    	for user in users:
-    		data.append([user.username, user.password])
+        elements.append(Paragraph(encabezado, styles['Heading1']))
+        data.append(['Nombre', 'Fecha nacimiento', 'Lugar nacimiento', 'Posición', 'Entidad'])
 
-    	table = Table(data)
-    	table.setStyle(TableStyle([('ALIGN',(1,1),(-2,-2),'RIGHT'),
+        if "Ranking" in encabezado:
+            for dep in lista:
+                data.append([dep.nombre, dep.fecha_nacim, dep.lugar_nacimiento, dep.ranking_nacional,
+                    dep.entidad.codigo])
+
+        table = Table(data)
+        table.setStyle(TableStyle([('ALIGN',(1,1),(-2,-2),'RIGHT'),
                        ('TEXTCOLOR',(1,1),(-2,-2),colors.red),
                        ('VALIGN',(0,0),(0,-1),'TOP'),
                        ('TEXTCOLOR',(0,0),(0,-1),colors.blue),
@@ -54,12 +59,12 @@ class MyPrint:
                        ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
                        ('BOX', (0,0), (-1,-1), 0.25, colors.black),
                        ]))
-    	elements.append(table)
 
-    	doc.build(elements)
-    	pdf = buffer.getvalue()
-    	buffer.close()
-    	return pdf
+        elements.append(table)
+        doc.build(elements)
+        pdf = buffer.getvalue()
+        buffer.close()
+        return pdf
 
 class MenuReportesGraficos(TemplateView):
 	template_name = 'reportes/menu_reportes_graficos.html'
@@ -75,6 +80,7 @@ class ReportesTablas(TemplateView):
 
 class ReporteRankingNacionalDeporte(TemplateView):
   template_name = 'reportes/ranking-nacional-deporte.html'
+  deporte_esc = None
 
   def get_context_data(self, **kwargs):
     context = super(ReporteRankingNacionalDeporte, self).get_context_data(**kwargs)
@@ -100,27 +106,43 @@ class ReporteRankingNacionalDeporte(TemplateView):
     ver_grupo = InformacionUsuario()
     grupo = ver_grupo.asignarGrupo(self.request.user)
     context[grupo] = grupo
-    print (request.POST)
 
-    deporte_seleccionado = request.POST['deporte']
+    if 'pdf' not in request.POST:
+      deporte_seleccionado = request.POST['deporte']
 
-    if len(deporte_seleccionado) > 0:
+      if len(deporte_seleccionado) > 0:
 
-      deporte_esc = Actividades.objects.get(codigo=deporte_seleccionado)
-      deportistas = Deportistas.objects.filter(deporte_practicado=deporte_esc).order_by("ranking_nacional")
+        self.deporte_esc = Actividades.objects.get(codigo=deporte_seleccionado)
+        deportistas = Deportistas.objects.filter(deporte_practicado=self.deporte_esc).order_by("ranking_nacional")
 
-      if len(deportistas) > 0:
-        context['deportistas'] = deportistas
-        context['deporte'] = deporte_esc
+        if len(deportistas) > 0:
+          context['deportistas'] = deportistas
+          context['deporte'] = self.deporte_esc
+
+        else:
+          context['vacio'] = 'No hay deportistas registrados para el deporte ' + self.deporte_esc.actividad
+          context['inicio'] = "inicio"
+          context['deportes'] = self.get_deportes()
 
       else:
-        context['vacio'] = 'No hay deportistas registrados para el deporte ' + deporte_esc.actividad
+        context['invalido'] = 'Debe seleccionar un deporte'
         context['inicio'] = "inicio"
         context['deportes'] = self.get_deportes()
 
     else:
-      context['invalido'] = 'Debe seleccionar un deporte'
-      context['inicio'] = "inicio"
-      context['deportes'] = self.get_deportes()
 
+      self.deporte_esc = Actividades.objects.get(codigo=int(request.POST['pdf']))  
+      deportistas = Deportistas.objects.filter(deporte_practicado=self.deporte_esc).order_by("ranking_nacional")
+
+      for deportista in deportistas:
+        print(deportista.nombre)
+
+      response = HttpResponse(content_type='application/pdf')
+      filename = 'ranking_nacional_' + self.deporte_esc.actividad + '.pdf'
+      encabezado = 'Ranking Nacional de ' + self.deporte_esc.actividad
+      buffer = BytesIO()
+      report = PDFReport(buffer, 'A4')
+      pdf = report.print_elements(deportistas, encabezado)
+      response.write(pdf)
+      return response
     return render(request, self.template_name, context)
