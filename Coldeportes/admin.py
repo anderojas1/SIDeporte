c963d807c4c3be44e5532ea0af6deeccafabc9ca
Anderson Enríquez Rojas <anderojas@new-host-2.home>
2016-02-15 00:12:39 -0500

    administración de actividades desde el admin de Django

--- Coldeportes/admin.py
+++ Coldeportes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ubicacion , Entidad, Escenarios, Deportistas, Dedicacion
+from .models import *
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(Entidad)
 admin.site.register(Escenarios)
 admin.site.register(Deportistas)
 admin.site.register(Dedicacion)
+admin.site.register(Actividades)
